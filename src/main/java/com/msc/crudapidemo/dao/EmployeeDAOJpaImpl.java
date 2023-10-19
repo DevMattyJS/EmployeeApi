444db861af620b966713df2e91cda416f1d1dafa
DevMattyJS <matej.sliacky@protonmail.com>
2023-10-19 17:01:28 +0200

    Implemented methods for CRUD operations

--- src/main/java/com/msc/crudapidemo/dao/EmployeeDAOJpaImpl.java
+++ src/main/java/com/msc/crudapidemo/dao/EmployeeDAOJpaImpl.java
@@ -24,4 +24,28 @@ public class EmployeeDAOJpaImpl implements EmployeeDAO {
         List<Employee> employees = query.getResultList();
         return employees;
     }
+
+    @Override
+    public Employee findById(int id) {
+
+        Employee employee = entityManager.find(Employee.class, id);
+        return employee;
+    }
+
+    // We don't use @Transactional here, because it's a good practice to apply transactional boundaries at the service layer
+    // It is a service layer's responsibility to manage transactional boundaries
+    @Override
+    public Employee save(Employee employee) {
+
+        // entityManager.merge() => insert or update the employee based on id
+        // if id == 0 => insert a new entry into db, otherwise update the existing entry
+        Employee dbEmployee = entityManager.merge(employee);
+        return dbEmployee;
+    }
+
+    @Override
+    public void deleteById(int id) {
+        Employee employee = findById(id);
+        entityManager.remove(employee);
+    }
 }
--- src/main/java/com/msc/crudapidemo/service/EmployeeServiceImpl.java
+++ src/main/java/com/msc/crudapidemo/service/EmployeeServiceImpl.java
@@ -6,6 +6,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+
+// Service layer class => it's an intermediate layer for custom business logic
+//                     => it can integrate data from multiple sources (DAO/repositories)
+//                     => it delegates the calls to DAO
 @Service
 public class EmployeeServiceImpl implements EmployeeService {
 
@@ -19,5 +23,20 @@ public class EmployeeServiceImpl implements EmployeeService {
     @Override
     public List<Employee> findAll() {
         return employeeDAO.findAll();
+    }
+
+    @Override
+    public Employee findById(int id) {
+        return employeeDAO.findById(id);
+    }
+
+    @Override
+    public Employee save(Employee employee) {
+        return employeeDAO.save(employee);
+    }
+
+    @Override
+    public void deleteById(int id) {
+        employeeDAO.deleteById(id);
     }
 }
